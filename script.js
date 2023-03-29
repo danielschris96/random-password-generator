bf921b70d51c550f7f94191265157f01485bd101
Chris Daniels <danielschris96@gmail.com>
2023-03-28 22:37:03 -0700

    added alert for if no character type is selected

--- script.js
+++ script.js
@@ -9,22 +9,27 @@ function writePassword() {
 
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
-var length = Number(prompt("How many characters do you want your password to be? (Min: 8, Max: 128)"));
+var length = prompt("How many characters do you want your password to be? (Min: 8, Max: 128)");
   if ((7 < length) && (129 > length)) {
-    console.log("Password will be " + length + " characters long");
+    // console.log("Password will be " + length + " characters long");
     uppercase = confirm("Do you want to include uppercase letters?\nOK= Yes\nCancel= No");
     lowercase = confirm("Do you want to include lowercase letters?\nOK= Yes\nCancel= No");
     numbers = confirm("Do you want to include numbers?\nOK= Yes\nCancel= No");
     specialCharacters = confirm("Do you want to include special characters?\nOK= Yes\nCancel= No");
-    console.log(length)
-    
+    generatePassword();
 
   }
   else alert("Character length must be 8 or more and 128 or less")
   passwordText.value = password;
+  return;
 
 }
 
+function generatePassword() {
+  if (!uppercase && !lowercase && !numbers && !specialCharacters) {
+    alert("Atleast one character type is required")
+  }
+}
 
 
 // Add event listener to generate button
